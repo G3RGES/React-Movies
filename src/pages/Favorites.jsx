7155e053feb9bbc3ec8c7ad792a4840877732e3d
G3RGES <georgios.nashaat@gmail.com>
2026-09-07 12:40:02 +0300

    Create Favorites.jsx

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,12 @@
+import "../css/Favorites.css";
+
+const Favorites = () => {
+  return (
+    <div className="favorites-empty">
+      <h2>Favorites</h2>
+      <p>You don't have any favorite movies yet</p>
+    </div>
+  );
+};
+
+export default Favorites;
