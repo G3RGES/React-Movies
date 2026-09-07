dad9d23e40edefea875665a426e1bb511d115cea
G3RGES <georgios.nashaat@gmail.com>
2026-09-07 12:40:00 +0300

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import "../css/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-logo">
+          <h3>Movie App</h3>
+        </Link>
+      </div>
+      <div className="navbar-links">
+        <Link to="/" className="navbar-link">
+          Home
+        </Link>
+        <Link to="/favorites" className="navbar-link">
+          Favorites
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
