536fa50f8a7362af2a567310d5cb4c9b116fde19
G3RGES <georgios.nashaat@gmail.com>
2026-09-09 05:42:12 +0300

    Update MovieContext.jsx

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-/* eslint-disable react-hooks/set-state-in-effect */
+
 import { useState, createContext, useContext, useEffect } from "react";
 
 const MovieContext = createContext();
