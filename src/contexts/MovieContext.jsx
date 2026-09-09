54e8231dccc8fc1ee6c056bc4efb5756822cb53f
G3RGES <georgios.nashaat@gmail.com>
2026-09-09 05:41:47 +0300

    Create MovieContext.jsx

---
+++ src/contexts/MovieContext.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react-hooks/set-state-in-effect */
+import { useState, createContext, useContext, useEffect } from "react";
+
+const MovieContext = createContext();
+
+export const useMovieContext = () => useContext(MovieContext);
+
+export const MovieProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState(() => {
+    const storedFavorites = localStorage.getItem("favorites");
+
+    return storedFavorites ? JSON.parse(storedFavorites) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
+  const addFavorite = (movie) => {
+    setFavorites([...favorites, movie]);
+    // setFavorites((prevFavorites) => [...prevFavorites, movie]);
+  };
+
+  const removeFavorite = (movieId) => {
+    setFavorites((prevFavorites) =>
+      prevFavorites.filter((movie) => movie.id !== movieId),
+    );
+  };
+
+  const isFavorite = (movieId) => {
+    return favorites.some((movie) => movie.id === movieId);
+  };
+
+  return (
+    <MovieContext.Provider
+      value={{
+        favorites,
+        addFavorite,
+        removeFavorite,
+        isFavorite,
+      }}
+    >
+      {children}
+    </MovieContext.Provider>
+  );
+};
